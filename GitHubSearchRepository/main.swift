161bc25db97897a804ac93713fcc5d637c7a8453
tomozip <dev.tomoki@gmail.com>
2018-05-25 13:36:15 +0900

    Initial Commit

---
+++ GitHubSearchRepository/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  GitHubSearchRepository
+//
+//  Created by 島田智貴 on 2018/05/25.
+//  Copyright © 2018年 hakusan-labo. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
